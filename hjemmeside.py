2e2b2be89ae7590e82b3c99091df2743bd708a7e
Bloodspear <143330693+Bloodspear@users.noreply.github.com>
2024-10-22 18:06:03 +0200

    Oppdaterte feil/la til flere def
    La til flere def når du skal drive å klikke på ulike enheter. Så kommer det riktig tekst opp.

--- hjemmeside.py
+++ hjemmeside.py
@@ -20,22 +20,32 @@ class Homepage:
         button1.place(x=50, y=50, width=100, height=100)
         button2 = tk.Button(self.left_frame, text="Tablet",bg='red',activebackground='green', command=self.button2_clicked)
         button2.place(x=200, y=50, width=100, height=100)
-        button3 = tk.Button(self.left_frame, text="Kjøleskap", bg='red', activebackground='green', command=self.button2_clicked)
+        button3 = tk.Button(self.left_frame, text="Kjøleskap", bg='red', activebackground='green', command=self.button3_clicked)
         button3.place(x=50, y=200, width=100, height=100)
-        button4 = tk.Button(self.left_frame, text="Telefon", bg='red', activebackground='green', command=self.button2_clicked)
+        button4 = tk.Button(self.left_frame, text="Printer", bg='red', activebackground='green', command=self.button4_clicked)
         button4.place(x=200, y=200, width=100, height=100)
-        button5 = tk.Button(self.left_frame, text="Printer", bg='red', activebackground='green', command=self.button2_clicked)
-        button5.place(x=200, y=200, width=100, height=100)
-        button6 = tk.Button(self.left_frame, text="Kamera", bg='red', activebackground='green', command=self.button2_clicked)
-        button6.place(x=50, y=350, width=100, height=100)
-        button7 = tk.Button(self.left_frame, text="Vifte", bg='red', activebackground='green', command=self.button2_clicked)
-        button7.place(x=200, y=350, width=100, height=100)
+        button5 = tk.Button(self.left_frame, text="Kamera", bg='red', activebackground='green', command=self.button5_clicked)
+        button5.place(x=50, y=350, width=100, height=100)
+        button6 = tk.Button(self.left_frame, text="Vifte", bg='red', activebackground='green', command=self.button6_clicked)
+        button6.place(x=200, y=350, width=100, height=100)
 
     def button1_clicked(self):
-        self.show_right_frame("You clicked Button 1")
+        self.show_right_frame("You clicked PC")
 
     def button2_clicked(self):
-        self.show_right_frame("You clicked Button 2")
+        self.show_right_frame("You clicked Tablet")
+
+    def button3_clicked(self):
+        self.show_right_frame("You clicked Kjøleskap")
+
+    def button4_clicked(self):
+        self.show_right_frame("You clicked Printer")
+
+    def button5_clicked(self):
+        self.show_right_frame("You clicked Kamera")
+
+    def button6_clicked(self):
+        self.show_right_frame("You clicked Vifte")
 
     def show_right_frame(self, text):
         # Hvis høyre ramme ikke er vist, pakk den ut
