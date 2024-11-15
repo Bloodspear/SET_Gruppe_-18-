f0af522d54168f97fd41221cdbfa388ebe5119d9
Bloodspear <143330693+Bloodspear@users.noreply.github.com>
2024-11-15 21:15:18 +0100

    Delte opp koden i mindre deler
    Dette er for å ha løst koblet kode og for å gjøre det mulig å jobbe med koden i ulike branches.

--- enhetskontroll.py
+++ enhetskontroll.py
@@ -1,1 +1,24 @@
-#
+class Enhetskontroll:
+    def init(self, homepage):
+        self.homepage = homepage
+
+    # temperatur +
+    def increase_temp(self):
+        self.temperature += 1
+        self.temp_label.config(text=f"Grader: {self.temperature} °C")
+
+    # temperatur -
+    def decrease_temp(self):
+        self.temperature -= 1
+        self.temp_label.config(text=f"Grader: {self.temperature} °C")
+
+    # lys av/på
+    def toggle_light(self):
+        if self.light_status == "Av":
+            self.light_status = "På"
+            self.farge = "gold"
+        else:
+            self.light_status = "Av"
+            self.farge = "gray40"
+        self.light_label.config(text=f"Lys: {self.light_status}")
+        self.light_button.config(bg=self.farge)
